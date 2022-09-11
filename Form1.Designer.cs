880e80e1795139e25f9361ed965989c65f4179a2
BlackB0w <71648098+BlackB0w@users.noreply.github.com>
2022-09-11 17:32:53 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace OutlineSelection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выйтиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.ImBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ImBox
+            // 
+            this.ImBox.Location = new System.Drawing.Point(0, 75);
+            this.ImBox.Name = "ImBox";
+            this.ImBox.Size = new System.Drawing.Size(660, 411);
+            this.ImBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ImBox.TabIndex = 0;
+            this.ImBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(843, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem,
+            this.выйтиToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // выйтиToolStripMenuItem
+            // 
+            this.выйтиToolStripMenuItem.Name = "выйтиToolStripMenuItem";
+            this.выйтиToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.выйтиToolStripMenuItem.Text = "Выйти";
+            this.выйтиToolStripMenuItem.Click += new System.EventHandler(this.выйтиToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Выделить Контуры";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(281, 46);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(379, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 7;
+            this.progressBar1.UseWaitCursor = true;
+            this.progressBar1.Visible = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(88, 7);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(63, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Прюитт";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(157, 7);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(62, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.Text = "Собель";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(225, 7);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(52, 17);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.Text = "Шарр";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Location = new System.Drawing.Point(685, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(146, 277);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Порог фильтрации/ Множитель яркости";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(54, 33);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(54, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 141);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(54, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(54, 251);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(54, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(26, 31);
+            this.trackBar1.Maximum = 256;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 240);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.TickFrequency = 16;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
+            this.radioButton4.Location = new System.Drawing.Point(15, 31);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(120, 17);
+            this.radioButton4.TabIndex = 12;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Порог фильтрации";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(15, 54);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(119, 17);
+            this.radioButton5.TabIndex = 12;
+            this.radioButton5.Text = "Вычисление цвета";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton4);
+            this.panel1.Controls.Add(this.radioButton5);
+            this.panel1.Location = new System.Drawing.Point(685, 325);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(146, 100);
+            this.panel1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 498);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ImBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "OutlineSelection";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ImBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выйтиToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox ImBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OutlineSelection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Bitmap OutlineSelection( Bitmap Im)// Метод выделения контуров
+        {
+            int h = Im.Height, w = Im.Width;
+            progressBar1.Visible = true;
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = h * w;
+            progressBar1.Step = 1;
+            for (int i = 0; i < w - 2; i++)// проход по изображению и наложение маски
+                for (int j = 0; j < h - 2; j++)
+                {
+                    float Gx = 0, Gy = 0;
+                    if (radioButton1.Checked)
+                    {// Градиенты x и y по оператору Прюитт
+                        Gx = -Im.GetPixel(i, j).GetBrightness() - Im.GetPixel(i + 1, j).GetBrightness() - Im.GetPixel(i + 2, j).GetBrightness() +
+                           Im.GetPixel(i, j + 2).GetBrightness() + Im.GetPixel(i + 1, j + 2).GetBrightness() + Im.GetPixel(i + 2, j + 2).GetBrightness();
+                        Gy = -Im.GetPixel(i, j).GetBrightness() - Im.GetPixel(i, j + 1).GetBrightness() - Im.GetPixel(i, j + 2).GetBrightness() +
+                           Im.GetPixel(i, j + 2).GetBrightness() + Im.GetPixel(i + 1, j + 2).GetBrightness() + Im.GetPixel(i + 2, j + 2).GetBrightness();
+                    }
+                    else if (radioButton2.Checked)
+                    {
+                        // Градиенты x и y по оператору Собеля
+                        Gx = -Im.GetPixel(i, j).GetBrightness() - 2 * Im.GetPixel(i + 1, j).GetBrightness() - Im.GetPixel(i + 2, j).GetBrightness() +
+                           Im.GetPixel(i, j + 2).GetBrightness() + 2 * Im.GetPixel(i + 1, j + 2).GetBrightness() + Im.GetPixel(i + 2, j + 2).GetBrightness();
+                        Gy = -Im.GetPixel(i, j).GetBrightness() - 2 * Im.GetPixel(i, j + 1).GetBrightness() - Im.GetPixel(i, j + 2).GetBrightness() +
+                           Im.GetPixel(i, j + 2).GetBrightness() + 2 * Im.GetPixel(i + 1, j + 2).GetBrightness() + Im.GetPixel(i + 2, j + 2).GetBrightness();
+                    }
+
+                    else if (radioButton3.Checked)
+                    {// Градиенты x и y по оператору Шарра
+                        Gx = -3 * Im.GetPixel(i, j).GetBrightness() - 10 * Im.GetPixel(i + 1, j).GetBrightness() - 3 * Im.GetPixel(i + 2, j).GetBrightness() +
+                          3 * Im.GetPixel(i, j + 2).GetBrightness() + 10 * Im.GetPixel(i + 1, j + 2).GetBrightness() + 3 * Im.GetPixel(i + 2, j + 2).GetBrightness();
+                        Gy = -3 * Im.GetPixel(i, j).GetBrightness() - 10 * Im.GetPixel(i, j + 1).GetBrightness() - 3 * Im.GetPixel(i, j + 2).GetBrightness() +
+                           3 * Im.GetPixel(i, j + 2).GetBrightness() + 10 * Im.GetPixel(i + 1, j + 2).GetBrightness() + 3 * Im.GetPixel(i + 2, j + 2).GetBrightness();
+                    }
+                    
+                    if (radioButton4.Checked)// пороговая фильтрация
+                    {
+                        double border = trackBar1.Value/10000;
+                        int G = Convert.ToInt32(Math.Sqrt(Math.Pow(Gx, 2) + Math.Pow(Gy, 2)));
+                        if (G > border)
+                            Im.SetPixel(i, j, Color.FromArgb(255, 255, 255));
+                        else Im.SetPixel(i, j, Color.FromArgb(0, 0, 0));
+                    }
+                    else if (radioButton5.Checked)// вычисление нового цвета на основе полученного градиента
+                    {
+                        double multiplier = trackBar1.Value;
+                        int G = Convert.ToInt32(Math.Sqrt(Math.Pow(Gx, 2) + Math.Pow(Gy, 2))*multiplier)%255;
+                        Im.SetPixel(i, j, Color.FromArgb(G, G, G));//установка нового цвета пикселя
+                    }
+                    progressBar1.PerformStep();
+                }
+            progressBar1.Visible = false;
+            progressBar1.Value = 0;
+            return Im;
+        }
+
+        private void UIMethodChange() {//изменение значений ползунка для регулирования заданного метода
+            if (radioButton4.Checked)
+            {
+                textBox1.Text = "0";
+                textBox3.Text = "1";
+                trackBar1.Minimum = 0;
+                trackBar1.Maximum = 10000;
+                trackBar1.TickFrequency = 1250;
+            }
+            else if (radioButton5.Checked)
+            {
+                textBox1.Text = "0";
+                textBox3.Text = "256";
+                trackBar1.Maximum = 256;
+                trackBar1.Minimum = 0;
+                trackBar1.TickFrequency = 16;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)//Установка заглушки при загрузке формы
+        {
+            ImBox.Image= Image.FromFile("C:\\PracticePics\\Stub.jpg");
+            UIMethodChange();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)//Загрузка изображения
+        {
+            openFileDialog1.Filter = "Jpg|*.jpg|Jpeg|*.jpeg";
+           if ( openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                ImBox.Image = Image.FromFile(openFileDialog1.FileName);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)//Вызов метода выделения контуров
+        {
+            ImBox.Image = OutlineSelection((ImBox.Image as Bitmap));
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)//Сохранение изображения
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                ImBox.Image.Save(saveFileDialog1.FileName);
+        }
+
+        private void выйтиToolStripMenuItem_Click(object sender, EventArgs e)// выход
+        {
+            this.Close();
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)// обновление интерфейса при смене метода
+        {
+            UIMethodChange();
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)// обновление интерфейса при смене метода
+        {
+            UIMethodChange();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)// отображение текущего выбранного на ползунке значения
+        {
+            if (radioButton4.Checked)
+                textBox2.Text = Convert.ToString(Convert.ToDouble(trackBar1.Value) / 10000);
+            else textBox2.Text = Convert.ToString(trackBar1.Value);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)// ввод значения пользователем
+        {
+            if (textBox2.Text != "")
+            {
+                if (radioButton4.Checked)
+                {
+                    if (Convert.ToInt32(Convert.ToDouble(textBox2.Text) * 10000) <= trackBar1.Maximum)
+                        trackBar1.Value = Convert.ToInt32(Convert.ToDouble(textBox2.Text) * 10000);
+                }
+                else if (radioButton5.Checked)
+                        if (Convert.ToInt32(textBox2.Text) <= trackBar1.Maximum)
+                            trackBar1.Value = Convert.ToInt32(textBox2.Text);
+            }
+        }
+    }
+}
